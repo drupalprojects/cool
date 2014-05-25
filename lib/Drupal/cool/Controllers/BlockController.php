e026f08db2201d8a0efe992f57ce6d0976cdf7b2
Pedro Rocha <pedro@minka.com.br>
2014-05-25 20:25:41 -0300

    New classes for Blocks and Forms as classes

---
+++ lib/Drupal/cool/Controllers/BlockController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\cool\Controllers;
+
+interface BlockController {
+
+  /**
+   * Path to be used by hook_info().
+   */
+  static public function getId();
+
+  /**
+   * Passed to hook_block_info().
+   */
+  static public function getAdminTitle();
+
+  /**
+   * Passed to hook_block_info().
+   */
+  static public function getDefinition();
+
+  /**
+   * Passed to hook_block_view().
+   */
+  static public function getSubject();
+
+  /**
+   * Passed to hook_block_view().
+   */
+  static public function getContent();
+}
